6ec1f9e062b735cd6541511a29348e8fd3a26004
unknown <cpe.cruz.marklester@gmail.com>
2023-09-21 09:58:54 +0800

    toggle menu close

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ toggleBtn.addEventListener("click", () => {
   toggleBtn.classList.toggle("open");
   headerNav.classList.toggle("open");
 });
+
+// const toggleBtn = document.querySelector(".toggle__menu");
+// toggleBtn.forEach((tab) => {
+//   tab.addEventListener("click", () => {
+//     removeActiveTab();
+//     tab.classList.add("active");
+//   });
+// });
+
+// function removeActiveTab() {
+//   toggleBtn.forEach((tab) => {
+//     tab.classList.remove("active");
+//   });
+// }
